9fa094933e7a0e52620e59c4e2199ff1d8abc65b
GiangHM <36400582+GiangHM@users.noreply.github.com>
2025-12-20 11:08:47 +0700

    * 'Line 53: Replaced the non-existent AddOpenApi() with AddSwaggerGen(), which is the standard method for adding OpenAPI/Swagger support in ASP.NET Core. This resolves the CS1061 error. Line 60: Replaced MapOpenApi() with UseSwagger() and UseSwaggerUI(), which are the standard middleware for serving OpenAPI/Swagger documentation in ASP.NET Core.' in file 'upload-doc\app\api\Program.cs'

--- upload-doc/app/api/Program.cs
+++ upload-doc/app/api/Program.cs
@@ -50,14 +50,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 // Add OpenAPI support
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapScalarApiReference();
 //}
 
